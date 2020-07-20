128ba4414998019bfa8db2fe9720faf30036ee35
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-07-20 18:42:55 +0200

    Dodanie podziękowania na dodatkowej stronie

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import Layout from "./components/UI/Layout/Layout";
-import ContactForm from "./components/ContactForm/ContactForm";
+import MainForm from "./components/ContactForm/MainForm";
 import FinalPage from "./components/FinalPage/FinalPage";
 
 const App = () => {
   return (
     <div>
       <Layout>
-        <ContactForm/>
-        <FinalPage/>
+          <Switch>
+              <Route path="/" exact component={MainForm}/>
+              <Route path="/final" component={FinalPage}/>
+          </Switch>
       </Layout>
     </div>
   );
---
+++ src/components/FinalPage/FinalPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './FinalPage.module.css';
+
+const FinalPage = () => (
+    <div className={classes.EndingInfo}>Thank you :)</div>
+)
+
+export default FinalPage;
